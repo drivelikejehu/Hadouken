77b9ece7f468f5f0de9685f84c504912fe6d24e8
Ricardo Russ <ricardoruss@live.com>
2020-06-19 17:19:41 -0400

    Login page is... skeletonized?

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,26 +1,63 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 class Login extends Component {
-    render() {
-        return (
-            <div className="container"> 
-            <h1 id="hadouken_header">Welcome to Hadouken!</h1> 
-                <div className="row">
-                    <div className="col">
-              WTF
-               <div className="row">
-                   <div className="col">
-                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Rerum beatae sunt mollitia ipsa similique cum facere, 
-                        autem fugiat ducimus odio, obcaecati consectetur 
-                        ea praesentium voluptatibus! Enim quas ullam placeat tempore!
-                   </div>
-                   </div>   
+  render() {
+    return (
+      <div className="container" id="hadouken_contain">
+        <h1>Welcome to Hadouken!</h1>
+        <div className="row">
+          <div className="col">
+            <div class="card">
+              <div class="card-body">
+                <form>
+                  <div className="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="exampleInputPassword1"
+                    />
+                  </div>
+                  <div className="form-group form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="exampleCheck1"
+                    />
+                    <label className="form-check-label" for="exampleCheck1">
+                      I'm not a robot.
+                    </label>
+                  </div>
+                  <div className="row justify-content-center">
+                      <div className="col-3">
+                      </div>
+                    <div className="col-3">
+                      <button type="submit" className="btn btn-primary">
+                        Log In
+                      </button>
+                    </div>
+                    <div className="col-3">
+                      <button className="btn btn-primary">Sign Up</button>
+                    </div>
+                    <div className="col-3">
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
-            </div>
-            </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
