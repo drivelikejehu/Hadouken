dc95b2cd7985af5f28765c88a0c648980eaefc59
John Kang <pollyfactor@hotmail.com>
2020-06-22 17:11:08 -0400

    added gamecard

--- client/src/pages/games/games.jsx
+++ client/src/pages/games/games.jsx
@@ -1,31 +1,33 @@
 import React, { Component } from "react";
+import GameCard from "../../components/GameCard/GameCard.jsx"
 
 class Games extends Component {
   render() {
     return (
       <div className="container text-center">
+        <form>
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Please enter employee search criteria"
+                  name="searchTerm"
+                  value="search term"
+                />
+              </div>
+            </div>
+            <div className="col-sm-2">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
         <div className="row">
           <div className="col">
-            <form>
-              <div className="row">
-                <div className="col">
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Please enter employee search criteria"
-                      name="searchTerm"
-                      value="search term"
-                    />
-                  </div>
-                </div>
-                <div className="col-sm-2">
-                  <button type="submit" className="btn btn-primary">
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </form>
+            <GameCard gamePic="https://upload.wikimedia.org/wikipedia/en/f/f5/Street_Fighter_II_Dash_%28flyer%29.png" gameName="SF2 CE"/>
           </div>
         </div>
       </div>
