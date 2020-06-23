256e026cc195556718228e847c26b4cce0424ad9
Ricardo Russ <ricardoruss@live.com>
2020-06-23 16:49:08 -0400

    half of sf2 char page completed

---
+++ client/src/components/CharCard/CharCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CharCard = (props) => {
+  return (
+    <div className="card justify-content-center" id="gameCard">
+      <img className="card-img-top" src={props.charPic} alt={props.charName} />
+      <div className="card-body">
+        <h5 className="card-title">{props.charName}</h5>
+        <p className="card-text">{props.charDesc}</p>
+        <a href="#" className="btn btn-primary" href={props.charURL}>
+          Go to {props.charName}'s character page?
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default CharCard;
