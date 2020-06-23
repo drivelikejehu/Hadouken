097fdce8d9208305ac5b914e12c872e0b6e77f2b
John Kang <pollyfactor@hotmail.com>
2020-06-23 13:54:31 -0400

    updated page with guile from state

--- client/src/pages/SF2/SF2.jsx
+++ client/src/pages/SF2/SF2.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import GameCard from "../../components/GameCard/GameCard.jsx";
+
 
 class SF2 extends Component {
-    render() {
+    state = {
+        gamePic:
+          "https://cdn.wikimg.net/en/strategywiki/images/c/c8/SSF2T_Guile.gif",
+        gameName: "Guile",
+      };
+      
+      render() {
         return (
-            <div>
-                sf2 page
+          <div className="container text-center">
+            <form>
+              <div className="row">
+                <div className="col">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Please enter employee search criteria"
+                      name="searchTerm"
+                      value="search term"
+                    />
+                  </div>
+                </div>
+                <div className="col-sm-2">
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+            <div className="row">
+              <div className="col">
+                <GameCard gamePic={this.state.gamePic} gameName={this.state.gameName} />
+              </div>
             </div>
+          </div>
         );
+      }
     }
-}
 
 export default SF2;
