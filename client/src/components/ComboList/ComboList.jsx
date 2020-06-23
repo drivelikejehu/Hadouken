7b752421b2732bfcd0dd38402e0a9625df9b4122
John Kang <pollyfactor@hotmail.com>
2020-06-23 13:54:09 -0400

    initial combolist structure

---
+++ client/src/components/ComboList/ComboList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ComboList extends Component {
+    render() {
+        return (
+            <div>
+                Combos
+            </div>
+        );
+    }
+}
+
+export default ComboList;
