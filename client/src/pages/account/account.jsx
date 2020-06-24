8f6ee61a2abe2883597e4c684e74949d46b7839d
John Kang <pollyfactor@hotmail.com>
2020-06-24 10:58:11 -0400

    addded state and put inputs into separate forms

--- client/src/pages/account/account.jsx
+++ client/src/pages/account/account.jsx
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
 
 class Account extends Component {
-  state = {};
+  state = {
+    avatar: "",
+    username: "",
+    birthday: "",
+  };
 
   handleSubmitAvatar = (event) => {
+    event.preventDefault();
+    this.setState({
+      avatar: ""
+    })
     console.log("Submit Avatar");
   };
 
   handleSubmitUsername = (event) => {
+    event.preventDefault();
+    this.setState({
+      username: ""
+    })
     console.log("Submit Username");
   };
 
   handleSubmitBirthday = (event) => {
+    event.preventDefault();
+    this.setState({
+      birthday: ""
+    })
     console.log("Submit Birthday");
   };
 
-  handleChange = (event) => {
+  handleInputChange = (event) => {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
@@ -33,33 +49,54 @@ class Account extends Component {
             <div className="col-4">
               <div className="card justify-content-center">
                 <div className="card-body">
-                    <div className="form-group">
-                      <input
-                        id="avatar"
-                        type="text"
-                        name="avatar"
-                        value="avatar"
-                      />
-                      <button className="btn btn-primary" onClick={this.handleSubmitAvatar}>Avatar</button>
-                    </div>
-                    <div className="form-group">
-                      <input
-                        id="username"
-                        type="text"
-                        name="username"
-                        value="username"
-                      />
-                      <button className="btn btn-primary" onClick={this.handleSubmitUsername}>Username</button>
-                    </div>
-                    <div className="form-group">
-                      <input
-                        id="birthday"
-                        type="text"
-                        name="birthday"
-                        value="birthday"
-                      />
-                      <button className="btn btn-primary" onClick={this.handleSubmitAvatar}>Birthday</button>
-                    </div>
+                  <div className="form">
+                    <input
+                      id="avatar"
+                      type="text"
+                      name="avatar"
+                      value={this.state.avatar}
+                      placeholder="Change you avatar"
+                      onChange={this.handleInputChange}
+                    />
+                    <button
+                      className="btn btn-primary"
+                      onClick={this.handleSubmitAvatar}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                  <div className="form">
+                    <input
+                      id="username"
+                      type="text"
+                      name="username"
+                      value={this.state.username}
+                      placeholder="Enter new username"
+                      onChange={this.handleInputChange}
+                    />
+                    <button
+                      className="btn btn-primary"
+                      onClick={this.handleSubmitBirthday}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                  <div className="form">
+                    <input
+                      id="birthday"
+                      type="text"
+                      name="birthday"
+                      value={this.state.birthday}
+                      placeholder="Enter new birthday"
+                      onChange={this.handleInputChange}
+                    />
+                    <button
+                      className="btn btn-primary"
+                      onClick={this.handleSubmitAvatar}
+                    >
+                      Submit
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
