89f2c75f087d95d976f1728c02dffea24829286f
John Kang <pollyfactor@hotmail.com>
2020-07-07 17:53:09 -0400

    updated card.css and characters.jsx

--- client/src/pages/SF2/Characters/SF2Guile.jsx
+++ client/src/pages/SF2/Characters/SF2Guile.jsx
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
-import ComboList from '../../../components/ComboList/ComboList'
-class SF2Guile extends Component { 
-    
-    state = {
-            char: "Guile",
-            combo1: "it's combo1",
-                combo2: "flash step",
-                combo3: "rising break"
-        };
-    render() {
-        return (
-            <div className="container text-center">
-                <div className="row">
-                    <div className="col">
-                duh nuh nuh na na
-                </div>
-                </div>
-                <ComboList
-                combo1={this.state.combo1}
-                combo2={this.state.combo2}
-                combo3={this.state.combo3}
-                char={this.state.char}
-                />
-            </div>
-        );
-    }
+import React, { Component } from "react";
+import ComboList from "../../../components/ComboList/ComboList";
+class SF2Guile extends Component {
+  state = {
+    char: "Guile",
+    combo1: "it's combo1",
+    combo2: "flash step",
+    combo3: "rising break",
+  };
+  render() {
+    return (
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">duh nuh nuh na na</div>
+        </div>
+        <ComboList
+          combo1={this.state.combo1}
+          combo2={this.state.combo2}
+          combo3={this.state.combo3}
+          char={this.state.char}
+        />
+      </div>
+    );
+  }
 }
 
 export default SF2Guile;
