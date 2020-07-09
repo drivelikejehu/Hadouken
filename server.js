43fe1e2822af9ae1c8bb51d20fc087656ccfa9d6
Ricardo Russ <ricardoruss@live.com>
2020-07-09 17:41:37 -0400

    table columns in userController

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-
+const { Sequelize } = require('sequelize');
+const db = require("./models");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -8,12 +9,16 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`);
+  });
 });
