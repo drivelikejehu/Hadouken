4033d8bf9f76b5cff04ea4c7f8ee860695c3f3d3
Ricardo Russ <ricardoruss@live.com>
2020-07-09 18:11:28 -0400

    post route of user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+
+router.post("/", (req, res)  => {
+const firstName = req.body.firstName.trim();
+const lastName = req.body.lastName.trim();
+const email = req.body.email.trim();
+const password = req.body.password.trim();
+const username = req.body.username.trim();
+const birthday = req.body.birthday.trim();
+const picURl = req.body.picURl.trim();
+db.User.create({ firstName, lastName, email, password, username, picURl, birthday })
+.then(() => {
+    res.json({
+        success: true
+    });
+}).catch((err) => {
+    console.log(err)})
+})
