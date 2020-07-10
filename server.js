ed3cb988850e695ce8774c283c307884d3a640cc
Ricardo Russ <ricardoruss@live.com>
2020-07-10 00:01:36 -0400

    making config.json

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const { Sequelize } = require('sequelize');
 const db = require("./models");
 const PORT = process.env.PORT || 3001;
-
+const UserController = require("./controllers/userController");
+const GameController = require("./controllers/gameController")
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +16,12 @@ app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
+});
+
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 db.sequelize.sync().then(function () {
